ee99e57a54e8322655df6d4096599bf49757528c
Phyllis <wong.phyllism@gmail.com>
2018-03-13 10:28:02 -0700

    Initial Commit

---
+++ What-Would-Bernie-Say/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What-Would-Bernie-Say
+//
+//  Created by Phyllis Wong on 3/13/18.
+//  Copyright © 2018 Phyllis Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
